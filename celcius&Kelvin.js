8fdb7b2d89c19435657ef1b6d79766e17a1afe7d
Rocio Lanfranconi <rocio@MacBook-Air-de-Rocio.local>
2022-10-15 14:29:42 +0200

    start project

---
+++ celcius&Kelvin.js
@@ -0,0 +1,17 @@
+document.getElementById('calcular').onclick = conversorTemp   //accion de hacer click en el boton conversar
+
+function conversorTemp(){
+   let celcius = document.getElementById("valorCelcius");
+   let kelvin = document.getElementById("valorKelvin");
+
+   if((celcius.value=="") && (kelvin.value=="")){
+   alert("ingresa valor");
+
+}else if(kelvin.value !== ""){
+celcius.value= (parseFloat(kelvin.value)-273.15).toFixed(1)
+
+}else if(celcius.value!== ""){
+   kelvin.value= (parseFloat(celcius.value)+273.15).toFixed(1 )
+}
+}
+
